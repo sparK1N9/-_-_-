7fb9d3a1229d4938589f99e60bdee3367ddd8f55
sparK1N9 <spacecqcenhancer@gmail.com>
2016-11-15 04:58:32 -0500

    XD

---
+++ Assets/scripts/Cam.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+    public GameObject[] Cameras;
+    int currentCam;
+
+    // Use this for initialization
+    void Start () {
+        DeactivateCameras();
+        Cameras[0].SetActive(true);
+        currentCam = 0;
+    }
+
+    public void activateCamera(int _camNumber)
+    {
+        Cameras[_camNumber - 1].SetActive(true);
+        currentCam = _camNumber - 1;
+    }
+
+    public void DeactivateCameras()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/scripts/trigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour {
+
+    public int camNumber;
+    public Cam controller;
+
+    void OnTriggerEnter(Collider other)
+    {
+        controller.DeactivateCameras();
+        controller.activateCamera(camNumber);
+    }
+}
